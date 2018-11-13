aa6bbe332888f9d237380b54d7c62095f5d0db0f
rocampocastro604 <44033716+rocampocastro604@users.noreply.github.com>
2018-11-13 04:23:39 -0500

    Composite py

---
+++ composite.py
@@ -0,0 +1,45 @@
+import abc
+
+
+class Component(metaclass=abc.ABCMeta):
+ 
+    @abc.abstractmethod
+    def operation(self):
+        pass
+
+
+class Composite(Component):
+
+    def __init__(self):
+        self._children = set()
+
+    def operation(self):
+        for child in self._children:
+            child.operation()
+
+    def add(self, component):
+        self._children.add(component)
+
+    def remove(self, component):
+        self._children.discard(component)
+
+
+class Leaf(Component):
+    """
+    Represent leaf objects in the composition. A leaf has no children.
+    Define behavior for primitive objects in the composition.
+    """
+
+    def operation(self):
+        pass
+
+
+def main():
+    leaf = Leaf()
+    composite = Composite()
+    composite.add(leaf)
+    composite.operation()
+
+
+if __name__ == "__main__":
+    main()
